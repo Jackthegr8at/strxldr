0d1f7415874ce9262e0e4e5a19901e2c452f3351
Jackthegr8at <je.t@kryptxt.ca>
2024-12-15 10:36:03 -0500

    price case back

--- src/index.tsx
+++ src/index.tsx
@@ -739,15 +739,6 @@ function Leaderboard() {
         {/* Statistics Dashboard */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
           <StatisticCard
-            title="STRX Price"
-            value={`$${strxPrice.toLocaleString(undefined, {
-              minimumFractionDigits: 4,
-              maximumFractionDigits: 4,
-            })}`}
-            tooltip="Current market price of STRX token, updated every 2 minutes from the blockchain oracle"
-          />
-
-          <StatisticCard
             title="Total Stakers"
             value={statistics?.totalUsers.toLocaleString()}
             tooltip="Total number of unique addresses that currently have STRX tokens staked"
@@ -791,6 +782,16 @@ function Leaderboard() {
             )}
             tooltip="The middle value of all stake amounts. 50% of stakers have more than this amount, 50% have less."
             onClick={() => toggleAmountDisplay('statistics', 'median')}
+          />
+
+          <StatisticCard
+            title="STRX Price"
+            value={`$${strxPrice.toLocaleString(undefined, {
+              minimumFractionDigits: 4,
+              maximumFractionDigits: 4,
+              useGrouping: true,
+            })}`}
+            tooltip="Current market price of STRX token, updated every 2 minutes from the blockchain oracle"
           />
         </div>
 
