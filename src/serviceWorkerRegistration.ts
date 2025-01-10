6d34a99fd14d162185f194ea412e48cf00867fd7
Jackthegr8at <je.t@kryptxt.ca>
2025-01-10 12:23:33 -0500

    pwa fix

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -1,7 +1,7 @@
 export function register() {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/workbox-sw.js`;
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
         navigator.serviceWorker.register(swUrl).catch(error => {
           console.error('Error during service worker registration:', error);
         });
