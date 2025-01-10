a11c78b9565ac4138fadf6c3d1312e1ea74723e1
Jackthegr8at <je.t@kryptxt.ca>
2025-01-10 13:48:09 -0500

    build

--- config-overrides.js
+++ config-overrides.js
@@ -6,48 +6,45 @@ module.exports = function override(config, env) {
     plugin => !plugin.constructor.name.includes('Workbox')
   );
 
-  // Always generate service worker for builds
-  config.plugins.push(
-    new WorkboxWebpackPlugin.GenerateSW({
-      swDest: 'service-worker.js', // Remove build/ prefix
-      clientsClaim: true,
-      skipWaiting: true,
-      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-      exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
-      modifyURLPrefix: {
-        // Remove leading slash from assets
-        '/': ''
-      },
-      runtimeCaching: [
-        {
-          urlPattern: ({ request }) => request.mode === 'navigate',
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'pages',
-            expiration: {
-              maxEntries: 50
+  if (env === 'production') {
+    config.plugins.push(
+      new WorkboxWebpackPlugin.GenerateSW({
+        swDest: 'service-worker.js',
+        clientsClaim: true,
+        skipWaiting: true,
+        include: [/\.html$/, /\.js$/, /\.css$/, /\.jpg$/, /\.png$/, /\.ico$/],
+        exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.mode === 'navigate',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'pages',
+              expiration: {
+                maxEntries: 50
+              }
+            }
+          },
+          {
+            urlPattern: ({ url }) => 
+              url.href.includes('api-xprnetwork-main.saltant.io') ||
+              url.href.includes('proton.eosusa.io') ||
+              url.href.includes('api-v3.raydium.io') ||
+              url.href.includes('api.bloks.io') ||
+              url.href.includes('api.dexscreener.com'),
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 5 * 60
+              }
             }
           }
-        },
-        {
-          urlPattern: ({ url }) => 
-            url.href.includes('api-xprnetwork-main.saltant.io') ||
-            url.href.includes('proton.eosusa.io') ||
-            url.href.includes('api-v3.raydium.io') ||
-            url.href.includes('api.bloks.io') ||
-            url.href.includes('api.dexscreener.com'),
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'api-cache',
-            expiration: {
-              maxEntries: 50,
-              maxAgeSeconds: 5 * 60
-            }
-          }
-        }
-      ]
-    })
-  );
+        ]
+      })
+    );
+  }
 
   return config;
 }
