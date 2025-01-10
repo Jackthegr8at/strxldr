6a0a232b41b85cf5b17c87dbfe3cd77f8ddb7da0
Jackthegr8at <je.t@kryptxt.ca>
2025-01-10 11:19:02 -0500

    vercel build test

--- config-overrides.js
+++ config-overrides.js
@@ -2,22 +2,20 @@ const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 
 module.exports = function override(config, env) {
-  if (env === 'production') {
-    // Remove any existing workbox plugins
-    config.plugins = config.plugins.filter(
-      plugin => !plugin.constructor.name.includes('Workbox')
-    );
+  // Remove any existing workbox plugins
+  config.plugins = config.plugins.filter(
+    plugin => !plugin.constructor.name.includes('Workbox')
+  );
 
-    // Add InjectManifest plugin
-    config.plugins.push(
-      new WorkboxWebpackPlugin.InjectManifest({
-        swSrc: path.resolve(__dirname, 'src/service-worker.ts'),
-        swDest: 'service-worker.js',
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-        exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
-      })
-    );
-  }
+  // Add InjectManifest plugin (removed the production check)
+  config.plugins.push(
+    new WorkboxWebpackPlugin.InjectManifest({
+      swSrc: path.resolve(__dirname, 'src/service-worker.ts'),
+      swDest: 'service-worker.js',
+      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
+    })
+  );
 
   return config;
 };
