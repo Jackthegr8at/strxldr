7a38ea52bd79a6f1d39e56f5570ee0e111619ab5
Jackthegr8at <je.t@kryptxt.ca>
2025-01-10 11:04:34 -0500

    build test

--- config-overrides.js
+++ config-overrides.js
@@ -2,21 +2,45 @@ const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 
 module.exports = function override(config, env) {
-  // Remove existing GenerateSW plugin
-  config.plugins = config.plugins.filter(
-    plugin => plugin.constructor.name !== 'GenerateSW'
-  );
+  if (env === 'production') {
+    // Remove any existing workbox plugins
+    config.plugins = config.plugins.filter(
+      plugin => !plugin.constructor.name.includes('Workbox')
+    );
 
-  // Add InjectManifest plugin
-  config.plugins.push(
-    new WorkboxWebpackPlugin.InjectManifest({
-      swSrc: path.resolve(__dirname, 'src/service-worker.ts'),
-      swDest: 'service-worker.js',
-      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-      exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
-      // Remove manifestTransforms
-    })
-  );
+    // Add GenerateSW plugin instead of InjectManifest
+    config.plugins.push(
+      new WorkboxWebpackPlugin.GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 60,
+                maxAgeSeconds: 24 * 60 * 60 // 24 hours
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/api/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 5 * 60 // 5 minutes
+              }
+            }
+          }
+        ]
+      })
+    );
+  }
 
   return config;
 };
